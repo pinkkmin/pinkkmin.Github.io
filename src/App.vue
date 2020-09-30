3e0b571153fb4b9aa444199d9e773c4a27a583c9
phenolpink <snake121387@gmail.com>
2020-09-30 23:51:08 +0800

    -f

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Descripttion: 
+ * @Date: 2020-09-30 17:03:56
+-->
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* -webkit-font-smoothing: antialiased; */
+  /*-moz-osx-font-smoothing: grayscale; */
+  color: #2c3e50;
+}
+</style>
